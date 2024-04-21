7c84e6884a4a6d5aab5ea392d2137c4c3478985e
fullm <matoma1_@naver.com>
2024-04-21 23:03:50 +0900

    CRUD 기능 인터페이스

---
+++ src/main/java/com/example/memorydb/db/DataRepository.java
@@ -0,0 +1,14 @@
+package com.example.memorydb.db;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DataRepository <T, ID> extends Repository<T, ID> {
+    T dave(T data); //create, update (id 찾아보고 없으면 save 있으면 update)
+
+    Optional<T> findById (ID id); //read
+    List<T> findAll();
+
+    //delete
+    void delete (ID id);
+}
