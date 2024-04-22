080db114a9c9c9bcdb14756c068fd33450971174
fullm <matoma1_@naver.com>
2024-04-22 13:02:16 +0900

    어노테이션

--- src/main/java/com/example/memorydb/book/db/entity/BookEntity.java
+++ src/main/java/com/example/memorydb/book/db/entity/BookEntity.java
@@ -1,9 +1,15 @@
 package com.example.memorydb.book.db.entity;
 
 import com.example.memorydb.entity.Entity;
+import lombok.*;
 
 import java.math.BigDecimal;
 //책의 속성
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookEntity extends Entity {
 
     private String name;
