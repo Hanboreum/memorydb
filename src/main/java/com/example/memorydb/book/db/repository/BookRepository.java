6b8cfb741f2437dc57fb923727fde0fadc80d5a9
fullm <matoma1_@naver.com>
2024-04-22 12:17:40 +0900

    주석

--- src/main/java/com/example/memorydb/book/db/repository/BookRepository.java
+++ src/main/java/com/example/memorydb/book/db/repository/BookRepository.java
@@ -5,6 +5,7 @@ import com.example.memorydb.db.SimpleDataRepository;
 import org.springframework.stereotype.Service;
 
 @Service //bin으로 스프링한테 등록 요청
+//BookRepository엔 bookentity에 대한 클래스 내용을 가진 정보가 담김 = 저장소
 public class BookRepository extends SimpleDataRepository<BookEntity, Long> {
     //id는 long타입
 }
