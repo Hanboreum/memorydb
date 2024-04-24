d2ecb3e47b461fbe0c215209ffbcd591ea835665
fullm <matoma1_@naver.com>
2024-04-24 11:24:31 +0900

    sql

--- book.sql
+++ book.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `user` (
+CREATE TABLE `book` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) COLLATE utf8mb4_bin NOT NULL,
-  `score` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  `category` varchar(45) COLLATE utf8mb4_bin NOT NULL,
+  `amount` decimal(10,0) DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
