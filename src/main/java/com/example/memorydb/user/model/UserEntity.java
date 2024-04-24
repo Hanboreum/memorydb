d43d8d0dcbc7e2a7d918656acccecbc843510fa4
fullm <matoma1_@naver.com>
2024-04-24 10:44:24 +0900

    Refactor : user 부분 db연결 jpa로 변환

--- src/main/java/com/example/memorydb/user/model/UserEntity.java
+++ src/main/java/com/example/memorydb/user/model/UserEntity.java
@@ -1,15 +1,25 @@
 package com.example.memorydb.user.model;
 
 
-import com.example.memorydb.entity.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
-@EqualsAndHashCode(callSuper = true)
+import java.math.BigDecimal;
+
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UserEntity extends Entity {
+@Entity(name = "user") //db와 매핑. user테이블과 연결
+public class UserEntity {
+
+    @Id //Ai 때문에
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //어떻게 생성, 관리 되는지
+    private Long id;
     private String name;
     private int score;
 }
